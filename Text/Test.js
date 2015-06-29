721e5768319e404f32b344840e0af70e528545e2
Ian Woskey <meat4every1@gmail.com>
2015-06-29 15:56:16 -0400

    ----

--- Text/Test.js
+++ Text/Test.js
@@ -1,6 +1,6 @@
 <!--
 document.write("<p>it worked</p>\
 <p>second bit of text worked!\
-<img title="Sven Front" src="Img/Golgotha Blog/P1_Plant.jpg">\
+<img title=\"Sven Front\" src=\"Img/Golgotha Blog/P1_Plant.jpg\">\
 ");
 -->
