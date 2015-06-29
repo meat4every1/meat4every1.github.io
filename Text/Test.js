9b626d100de07b62039e4c8f6ee69b5165f7851c
Ian Woskey <meat4every1@gmail.com>
2015-06-29 15:08:29 -0400

    ----

--- Text/Test.js
+++ Text/Test.js
@@ -1,11 +1,3 @@
 <!--
-document.write("
-
-    <h1>Add your HTML code here</h1>
-
-     <p>Notice however, that you have to escape LF's with a '\', just like
-        demonstrated in this code listing.
-    </p>
-
-");
+document.write("<p>it worked</p>");
 -->
