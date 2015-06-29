eab4cc98d8108ef07e1696340b45177132f25d97
Ian Woskey <meat4every1@gmail.com>
2015-06-29 17:22:08 -0400

    Golgotha Blog external P1 test

---
+++ Text/GB_P1.js
@@ -0,0 +1,19 @@
+<!--
+document.write("<p>\
+		At the onset there was a strong plan for our design philosophy. The goal is to create a powerful dark atmosphere that adds to our core themes of overcoming the terror of the unknown to embrace the thrill of discovery. At the same time our art style is somewhat filtered by more cartoony and lighthearted elements pulled from the design sense of hiyao miyazaki, Windwaker, Over The Garden Wall, etc.\ 
+	</p>\
+	<div>\
+		<ul class="imgRow">\
+			<li><img title="Sven Front" src="Img/Golgotha Blog/P1_Plant.jpg"></li>\
+			<li><img title="Sven Back" src="Img/Golgotha Blog/P1_Cent.jpg"></li>\
+		</ul>\
+	</div>\
+	<img class="fullImage" src="Img/Golgotha Blog/P1_PC1.jpg">\
+	<div>\
+		<ul class="imgRow">
+			<li><img title="Sven Front" src="Img/Golgotha Blog/P1_PC2.jpg"></li>\
+			<li><img title="Sven Back" src="Img/Golgotha Blog/P1_Tent.jpg"></li>\
+		</ul>\
+	</div>\
+");
+-->
