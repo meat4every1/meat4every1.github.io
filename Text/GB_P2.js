feb853cbe74914eca1254b4614b73343a05fd5d3
Ian Woksey <meat4every1@gmail.com>
2015-07-24 16:04:19 -0400

    adding post 2

---
+++ Text/GB_P2.js
@@ -0,0 +1,15 @@
+<!--
+document.write("<div>\
+		<ul class=\"cbImgRow\">\
+			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_1.jpg\"> <img title=\"PC1\" src=\"Img/Golgotha Blog/P2_CB1.jpg\"> </a> </li>\
+			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_2.jpg\"> <img title=\"PC2\" src=\"Img/Golgotha Blog/P2_CB2.jpg\"> </a> </li>\
+			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_3.jpg\"> <img class=\"PC3\" src=\"Img/Golgotha Blog/P2_CB3.jpg\"> </a> </li>\
+			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_4.jpg\"> <img title=\"PC4\" src=\"Img/Golgotha Blog/P2_CB4.jpg\"> </a> </li>\	
+		</ul>\
+		<p>asdas</p>\
+		<script>\
+            jQuery(\"a.P1_Gallery\").colorbox({rel:\'P1_Gallery\', width:\"60%\", height:\"90%\"});\
+        </script>\
+	</div>\
+");
+-->
