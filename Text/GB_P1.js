d7204fff752a80ce748e1c34fc14740bccbd6944
Ian Woksey <meat4every1@gmail.com>
2015-07-24 13:00:14 -0400

    ----

--- Text/GB_P1.js
+++ Text/GB_P1.js
@@ -7,7 +7,7 @@ document.write("<div>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_PC2.jpg\"> <img title=\"Sven Front\" src=\"Img/Golgotha Blog/P1_CBPC2.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_Tent.jpg\"><img title=\"Sven Back\" src=\"Img/Golgotha Blog/P1_CBTent.jpg\"> </a> </li>\
 		</ul>\
-		<p>At the onset there was a strong plan for our design philosophy. The goal is to create a powerful dark atmosphere that adds to our core themes of overcoming the terror of the unknown to embrace the thrill of discovery. At the same time our art style is somewhat filtered by more cartoony and lighthearted elements pulled from the design sense of hiyao miyazaki, Windwaker, Over The Garden Wall, etc.</p>\
+		<p>At the onset there was a strong plan for the art direction. The goal is to create a powerful dark atmosphere that adds to our core themes of overcoming the terror of the unknown to embrace the thrill of discovery. At the same time our art style is somewhat filtered by more cartoony and lighthearted elements pulled from the design sense of hiyao miyazaki, Windwaker, Over The Garden Wall, etc.</p>\
 		<script>\
             jQuery(\"a.P1_Gallery\").colorbox({rel:\'P1_Gallery\', width:\"60%\", height:\"90%\"});\
         </script>\
