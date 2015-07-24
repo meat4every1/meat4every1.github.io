837958f58cbd89579a548c83fef25bb6dce3ac79
Ian Woksey <meat4every1@gmail.com>
2015-07-24 16:32:15 -0400

    ----

--- Text/GB_P1.js
+++ Text/GB_P1.js
@@ -3,7 +3,7 @@ document.write("<div>\
 		<ul class=\"cbImgRow\">\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_Plant.jpg\"> <img title=\"Plant Monster\" src=\"Img/Golgotha Blog/P1_CBPlant.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_Cent.jpg\"> <img title=\"Sven Back\" src=\"Img/Golgotha Blog/P1_CBCent.jpg\"> </a> </li>\
-			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_PC1.jpg\"> <img class=\"fullImage\" src=\"Img/Golgotha Blog/P1_CBPC1.jpg\"> </a> </li>\
+			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_PC1.jpg\"> <img title=\"PC1\" src=\"Img/Golgotha Blog/P1_CBPC1.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_PC2.jpg\"> <img title=\"Sven Front\" src=\"Img/Golgotha Blog/P1_CBPC2.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_Tent.jpg\"><img title=\"Sven Back\" src=\"Img/Golgotha Blog/P1_CBTent.jpg\"> </a> </li>\
 		</ul>\
