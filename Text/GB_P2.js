edbf1020e4c1d9d93568d078eec0321e20fd3ad8
Ian Woksey <meat4every1@gmail.com>
2015-07-24 17:48:45 -0400

    ----

--- Text/GB_P2.js
+++ Text/GB_P2.js
@@ -6,7 +6,7 @@ document.write("\
 			<li><a class=\"P2_Gallery\" href=\"Img/Golgotha Blog/P2_2.jpg\"> <img title=\"PC2\" src=\"Img/Golgotha Blog/P2_CB2.jpg\"> </a> </li>\
 			<li><a class=\"P2_Gallery\" href=\"Img/Golgotha Blog/P2_3.jpg\"> <img title=\"PC3\" src=\"Img/Golgotha Blog/P2_CB3.jpg\"> </a> </li>\
 			<li><a class=\"P2_Gallery\" href=\"Img/Golgotha Blog/P2_4.jpg\"> <img title=\"PC4\" src=\"Img/Golgotha Blog/P2_CB4.jpg\"> </a> </li> </ul>\
-		<p>asdas</p>\
+		<p></p>\
 		<script>jQuery(\"a.P2_Gallery\").colorbox({rel:\'P2_Gallery\', width:\"60%\", height:\"90%\"});</script>\
 	</div>\
 ");
--- Text/GB_P6.js
+++ Text/GB_P6.js
@@ -6,7 +6,7 @@ document.write("\
 			<li><a class=\"P6_Gallery\" href=\"Img/Golgotha Blog/P6_2.jpg\"> <img title=\"PC2\" src=\"Img/Golgotha Blog/P6_CB2.jpg\"> </a> </li>\
 			<li><a class=\"P6_Gallery\" href=\"Img/Golgotha Blog/P6_3.jpg\"> <img title=\"PC3\" src=\"Img/Golgotha Blog/P6_CB3.jpg\"> </a> </li>\
 			<li><a class=\"P6_Gallery\" href=\"Img/Golgotha Blog/P6_4.jpg\"> <img title=\"PC4\" src=\"Img/Golgotha Blog/P6_CB4.jpg\"> </a> </li> </ul>\
-		<p>asdas</p>\
+		<p></p>\
 		<script>jQuery(\"a.P6_Gallery\").colorbox({rel:\'P6_Gallery\', width:\"60%\", height:\"90%\"});</script>\
 	</div>\
 ");
