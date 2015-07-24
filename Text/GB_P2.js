01f30226f99ea19b0052efeff05928901ad00695
Ian Woksey <meat4every1@gmail.com>
2015-07-24 16:20:25 -0400

    ----

--- Text/GB_P2.js
+++ Text/GB_P2.js
@@ -4,7 +4,7 @@ document.write("<div>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_1.jpg\"> <img title=\"PC1\" src=\"Img/Golgotha Blog/P2_CB1.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_2.jpg\"> <img title=\"PC2\" src=\"Img/Golgotha Blog/P2_CB2.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_3.jpg\"> <img class=\"PC3\" src=\"Img/Golgotha Blog/P2_CB3.jpg\"> </a> </li>\
-			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_4.jpg\"> <img title=\"PC4\" src=\"Img/Golgotha Blog/P2_CB4.jpg\"> </a> </li>\	
+			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P2_4.jpg\"> <img title=\"PC4\" src=\"Img/Golgotha Blog/P2_CB4.jpg\"> </a> </li>\	\
 		</ul>\
 		<p>asdas</p>\
 		<script>\
