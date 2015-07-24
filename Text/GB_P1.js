8d39d658cac59f0f93dfa459d634a233cd7179e7
Ian Woksey <meat4every1@gmail.com>
2015-07-23 21:04:12 -0400

    Colorbox row reformat

--- Text/GB_P1.js
+++ Text/GB_P1.js
@@ -1,7 +1,7 @@
 <!--
 document.write("<p>At the onset there was a strong plan for our design philosophy. The goal is to create a powerful dark atmosphere that adds to our core themes of overcoming the terror of the unknown to embrace the thrill of discovery. At the same time our art style is somewhat filtered by more cartoony and lighthearted elements pulled from the design sense of hiyao miyazaki, Windwaker, Over The Garden Wall, etc.</p>\
 	<div>\
-		<ul class=\"imgRow\">\
+		<ul class=\"cbImgRow\">\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_Plant.jpg\"> <img title=\"Plant Monster\" src=\"Img/Golgotha Blog/P1_Plant.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_Cent.jpg\"> <img title=\"Sven Back\" src=\"Img/Golgotha Blog/P1_Cent.jpg\"> </a> </li>\
 			<li><a class=\"P1_Gallery\" href= \"Img/Golgotha Blog/P1_PC1.jpg\"> <img class=\"fullImage\" src=\"Img/Golgotha Blog/P1_PC1.jpg\"> </a> </li>\
