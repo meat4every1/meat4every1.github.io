69ce1abc5a29d7d8fc75d6330d628f8229dcb1c8
Ian Woksey <meat4every1@gmail.com>
2016-04-08 19:06:33 -0400

    Rakshaza Gallery and phead fix

---
+++ Text/Port_Gal_ROR.js
@@ -0,0 +1,12 @@
+<!--
+document.write("\
+	<div>\
+		<ul class=\"cbImgRow\">\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR1_Full.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR1_CBFull.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR2_Portion.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR2_CBPortion.png\"> </a> </li>\
+			 </ul>\
+		<p></p>\
+		<script>jQuery(\"a.ROR_Gallery\").colorbox({rel:\'ROR_Gallery\', width:\"60%\", height:\"90%\"});</script>\
+	</div>\
+");
+-->
