819e3fbfb79b6eb83c197d15255022081610b190
Ian Woksey <meat4every1@gmail.com>
2016-04-08 19:14:01 -0400

    ROR .js gallery reformat test (first image outside of UL)

--- Text/Port_Gal_ROR.js
+++ Text/Port_Gal_ROR.js
@@ -1,8 +1,8 @@
 <!--
 document.write("\
 	<div>\
+		<a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR1_Full.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR1_CBFull.png\"> </a>\
 		<ul class=\"cbImgRow\">\
-			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR1_Full.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR1_CBFull.png\"> </a> </li>\
 			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR2_Portion.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR2_CBPortion.png\"> </a> </li>\
 			 </ul>\
 		<p></p>\
