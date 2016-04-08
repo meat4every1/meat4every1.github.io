239ca90eb35d46353a1c84be7fa029331163ed4d
Ian Woksey <meat4every1@gmail.com>
2016-04-08 19:28:19 -0400

    ROR Full Gallery

--- Text/Port_Gal_ROR.js
+++ Text/Port_Gal_ROR.js
@@ -1,9 +1,14 @@
 <!--
 document.write("\
 	<div>\
+	<div class=\"cbImgRow\">\
 		<a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR1_Full.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR1_CBFull.png\"> </a>\
+	</div>\
 		<ul class=\"cbImgRow\">\
 			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR2_Portion.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR2_CBPortion.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR3_Axe.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR3_CBAxe.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR4_Face.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR4_CBFace.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR5_Fire.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR5_CBFire.png\"> </a> </li>\
 			 </ul>\
 		<p></p>\
 		<script>jQuery(\"a.ROR_Gallery\").colorbox({rel:\'ROR_Gallery\', width:\"60%\", height:\"90%\"});</script>\
