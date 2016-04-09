ce48c90ea80620a19d119df5aa401ed8e9fce9e6
Ian Woksey <meat4every1@gmail.com>
2016-04-09 18:48:31 -0400

    --

--- Text/Port_Gal_Souls.js
+++ Text/Port_Gal_Souls.js
@@ -3,8 +3,8 @@ document.write("\
 	<div>\
 		<ul class=\"cbImgRow\">\
 			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls1_BB.png\"> <img title=\"Doll\" src=\"Img/Content Posts/Souls1_CBBB.png\"> </a> </li>\
-			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls1_Solair.png\"> <img title=\"Solair\" src=\"Img/Content Posts/Souls1_CBSolair.png\"> </a> </li>\
-			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls1_WL.png\"> <img title=\"The White Lady\" src=\"Img/Content Posts/Souls1_CBWL.png\"> </a> </li>\
+			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls2_Solair.png\"> <img title=\"Solair\" src=\"Img/Content Posts/Souls2_CBSolair.png\"> </a> </li>\
+			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls3_WL.png\"> <img title=\"The White Lady\" src=\"Img/Content Posts/Souls3_CBWL.png\"> </a> </li>\
 			 </ul>\
 		<p></p>\
 		<script>jQuery(\"a.ROR_Gallery\").colorbox({rel:\'ROR_Gallery\', width:\"60%\", height:\"90%\"});</script>\
