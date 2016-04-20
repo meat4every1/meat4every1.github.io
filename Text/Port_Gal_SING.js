ea0996e2726c7a4553256e601d389419332c0ff0
Ian Woksey <meat4every1@gmail.com>
2016-04-20 19:30:48 -0400

    Singularity JS\colorbox Test

---
+++ Text/Port_Gal_SING.js
@@ -0,0 +1,26 @@
+<!--
+document.write("\
+	<div>\
+		<p> Singularity was the working title of an exploratory prototype project between myself and Bobby Speck. This portion of the project was based on a high concept mobile game idea, and focused on determining whether or not this game was a viable production based on our time and resource limitations. The high concept was to create a puzzle game where players had little to no direct control of in game agents. Players would instead act through Ai built using a rudimentary block based coding system similar to that seen in educational languages like scratch. These Ai agents would interact with the world on your behalf and be built with increasingly complex code libraries to solve increasingly complex puzzles.  The purpose of this gameplay was to educate players on basic coding concepts, if statements, loops etc. in directed sandbox environments. The target audience was school age people with an interest in coding but no educational outlet to experiment with these interests. We wanted to give these young people a sense of ownership over coding concepts, by linking these concepts directly with their own interactive creations.\
+		</p>\
+	<p>During this exploratory prototyping phase we developed aspects of the UI and game world to functional completion, and created a robust plan for required development. Upon reaching this point we felt we had a full understanding of the project's complexity and requirements. Analyzing the needs of the project, the timeline of development to this point, and the expected timeline for prototype completion, we decided that our development goals would be better served by redirecting towards a smaller scale project.  We have high hopes for the future of Singularity, and fully intend to reexamine it if and when we have more resources we can leverage to address development concerns.  We have since redirected our efforts towards serialized storytelling in our new Webcomic, and plan to do another small scale game prototype focused on pipeline design.\
+	</p<\
+	<p>Concept art at early stage of development, showing the hub character that represents the player, the expected game world style, and plans for coding interface.</p>\
+	<ul class=\"cbImgRow\">\
+		<a class=\"SING_Gallery\" href=\"Img/Content Posts/SI1_UI.png\"> <img title=\"SI1\" src=\"Img/Content Posts/SI1_CBUI.png\"> </a>\
+	</ul>\
+	<p>Concept art depicting the planned aesthetics for fog of war in Singularity.  Fog of war would allow us make sure every puzzle contained at least 2 phases, discovery and action.</p>\
+	<ul class=\"cbImgRow\">\
+		<a class=\"SING_Gallery\" href=\"Img/Content Posts/SI2_FW.png\"> <img title=\"SI2\" src=\"Img/Content Posts/SI2_CBFW.png\"> </a>\
+	</ul>\
+		<ul class=\"cbImgRow\">\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR2_Portion.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR2_CBPortion.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR3_Axe.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR3_CBAxe.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR4_Face.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR4_CBFace.png\"> </a> </li>\
+			<li><a class=\"ROR_Gallery\" href=\"Img/Content Posts/ROR5_Fire.png\"> <img title=\"PC1\" src=\"Img/Content Posts/ROR5_CBFire.png\"> </a> </li>\
+			 </ul>\
+		<p></p>\
+		<script>jQuery(\"a.ROR_Gallery\").colorbox({rel:\'ROR_Gallery\', width:\"60%\", height:\"90%\"});</script>\
+	</div>\
+");
+-->
