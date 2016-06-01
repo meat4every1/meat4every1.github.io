ccc238fc80c3196b354e90c45ddc1d0267a5c7e9
Ian Woksey <meat4every1@gmail.com>
2016-06-01 04:26:39 -0400

    --

--- Text/Port_Gal_Souls.js
+++ Text/Port_Gal_Souls.js
@@ -5,7 +5,7 @@ document.write("\
 			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls1_BB.png\"> <img title=\"Doll\" src=\"Img/Content Posts/Souls1_CBBB.png\"> </a> </li>\
 			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls2_Solair.png\"> <img title=\"Solair\" src=\"Img/Content Posts/Souls2_CBSolair.png\"> </a> </li>\
 			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls3_WL.jpg\"> <img title=\"The White Lady\" src=\"Img/Content Posts/Souls3_CBWL.jpg\"> </a> </li>\
-			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls4_FK.jpg\"> <img title=\"The White Lady\" src=\"Img/Content Posts/Souls4_CBWL.jpg\"> </a> </li>\
+			<li><a class=\"Souls_Gallery\" href=\"Img/Content Posts/Souls4_FK.jpg\"> <img title=\"The White Lady\" src=\"Img/Content Posts/Souls4_CBFK.jpg\"> </a> </li>\
 		</ul>\
 		<p></p>\
 		<script>jQuery(\"a.Souls_Gallery\").colorbox({rel:\'Souls_Gallery\', width:\"60%\", height:\"90%\"});</script>\
